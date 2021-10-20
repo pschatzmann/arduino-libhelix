4a61d08549b58e2674e63f60ca2e85ce6a3555aa
Phil Schatzmann <phil.schatzmann@gmail.com>
2021-10-20 14:00:25 +0200

    compile warning for esp8266

--- src/helix_pgm.h
+++ src/helix_pgm.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#if defined(ARDUINO) && __has_include(<pgm_space>)
-#include <pgm_space.h>
+#ifdef ESP8266
+#  include "pgmspace.h"
+#elif defined(ARDUINO) && __has_include(<pgm_space.h>)
+#  include <pgm_space.h>
 #else
-
-#define PROGMEM
-#define pgm_read_byte(addr) (*(const unsigned char *)(addr))
-#define pgm_read_word(addr) (*(const unsigned short *)(addr))
-
+#  define PROGMEM
+#  define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+#  define pgm_read_word(addr) (*(const unsigned short *)(addr))
 #endif
