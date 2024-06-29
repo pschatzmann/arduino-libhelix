5b20cc13935ddcc8447a119108ac81fba569a619
pschatzmann <phil.schatzmann@gmail.com>
2024-06-29 12:05:49 +0200

    doxygen

--- docs/html/search/all_a.js
+++ docs/html/search/all_a.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['setaudioinfo_18',['setAudioInfo',['../classlibhelix_1_1_a_a_c_decoder_helix.html#abdfad65dca1bff4b7d7d82566540d0fd',1,'libhelix::AACDecoderHelix']]],
-  ['setdelay_19',['setDelay',['../classlibhelix_1_1_common_helix.html#ac62f121f7be3dc1b1cf80d983319efe9',1,'libhelix::CommonHelix']]],
-  ['setmaxframesize_20',['setMaxFrameSize',['../classlibhelix_1_1_common_helix.html#a28a81c90298d1dbc107b2b799ce3fed2',1,'libhelix::CommonHelix']]],
-  ['setmaxpcmsize_21',['setMaxPCMSize',['../classlibhelix_1_1_common_helix.html#ad81283a33815f2c1b915a4c00393e22d',1,'libhelix::CommonHelix']]],
-  ['setraw_22',['setRaw',['../classlibhelix_1_1_common_helix.html#a08a5f32d0f3b46fb8fd84bb4a35ff51e',1,'libhelix::CommonHelix']]]
+  ['setmaxframesize_19',['setMaxFrameSize',['../classlibhelix_1_1_common_helix.html#a28a81c90298d1dbc107b2b799ce3fed2',1,'libhelix::CommonHelix']]],
+  ['setmaxpcmsize_20',['setMaxPCMSize',['../classlibhelix_1_1_common_helix.html#ad81283a33815f2c1b915a4c00393e22d',1,'libhelix::CommonHelix']]],
+  ['setminframebuffersize_21',['setMinFrameBufferSize',['../classlibhelix_1_1_common_helix.html#a843186aabc424e76ca263145917640d5',1,'libhelix::CommonHelix']]],
+  ['setreference_22',['setReference',['../classlibhelix_1_1_common_helix.html#a48a996faa5a49aa6c397c62c3731a978',1,'libhelix::CommonHelix']]]
 ];
--- docs/html/search/functions_9.js
+++ docs/html/search/functions_9.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['setaudioinfo_44',['setAudioInfo',['../classlibhelix_1_1_a_a_c_decoder_helix.html#abdfad65dca1bff4b7d7d82566540d0fd',1,'libhelix::AACDecoderHelix']]],
-  ['setdelay_45',['setDelay',['../classlibhelix_1_1_common_helix.html#ac62f121f7be3dc1b1cf80d983319efe9',1,'libhelix::CommonHelix']]],
-  ['setmaxframesize_46',['setMaxFrameSize',['../classlibhelix_1_1_common_helix.html#a28a81c90298d1dbc107b2b799ce3fed2',1,'libhelix::CommonHelix']]],
-  ['setmaxpcmsize_47',['setMaxPCMSize',['../classlibhelix_1_1_common_helix.html#ad81283a33815f2c1b915a4c00393e22d',1,'libhelix::CommonHelix']]],
-  ['setraw_48',['setRaw',['../classlibhelix_1_1_common_helix.html#a08a5f32d0f3b46fb8fd84bb4a35ff51e',1,'libhelix::CommonHelix']]]
+  ['setmaxframesize_45',['setMaxFrameSize',['../classlibhelix_1_1_common_helix.html#a28a81c90298d1dbc107b2b799ce3fed2',1,'libhelix::CommonHelix']]],
+  ['setmaxpcmsize_46',['setMaxPCMSize',['../classlibhelix_1_1_common_helix.html#ad81283a33815f2c1b915a4c00393e22d',1,'libhelix::CommonHelix']]],
+  ['setminframebuffersize_47',['setMinFrameBufferSize',['../classlibhelix_1_1_common_helix.html#a843186aabc424e76ca263145917640d5',1,'libhelix::CommonHelix']]],
+  ['setreference_48',['setReference',['../classlibhelix_1_1_common_helix.html#a48a996faa5a49aa6c397c62c3731a978',1,'libhelix::CommonHelix']]]
 ];
